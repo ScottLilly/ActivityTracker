b8aa1a5ef708d7db10aa809cbdfb46be019fc235
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2021-09-24 20:19:34 -0500

    Added Program class, to let user customize which programs are logged and how their name is displayed in the UI.

---
+++ ActivityTracker.Models/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ActivityTracker.Models
+{
+    public class Program
+    {
+        public string ProcessName { get; set; }
+        public string DisplayName { get; set; }
+        public bool ShouldLog { get; set; }
+    }
+}
