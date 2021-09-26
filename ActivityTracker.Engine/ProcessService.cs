4154644bff50ea8308467237ad6b4ec6a798a478
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2021-09-26 11:15:52 -0500

    Refactoring

--- ActivityTracker.Engine/ProcessService.cs
+++ ActivityTracker.Engine/ProcessService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
+using ActivityTracker.Models;
+using ActivityTracker.Repository;
 
 namespace ActivityTracker.Engine
 {
@@ -12,6 +15,9 @@ namespace ActivityTracker.Engine
         [DllImport("user32.dll")]
         private static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
 
+        private static DataRepository s_dataRepository =
+            DataRepository.GetInstance();
+
         public static Process GetActiveProcess()
         {
             IntPtr hwnd = GetForegroundWindow();
@@ -19,6 +25,44 @@ namespace ActivityTracker.Engine
             GetWindowThreadProcessId(hwnd, out uint pid);
 
             return Process.GetProcessById((int)pid);
+        }
+
+        public static void RecordProgramFocusSwitch()
+        {
+            using Process process = GetActiveProcess();
+
+            if(process == null)
+            {
+                EndCurrentProgramFocus();
+                return;
+            }
+
+            // Don't add an entry if we are "switching" to the same process
+            // or switching to a program with no title (window)
+            if (s_dataRepository.LogEntries.LastOrDefault()?.ProcessId == process.Id ||
+               string.IsNullOrWhiteSpace(process.MainWindowTitle))
+            {
+                return;
+            }
+
+            EndCurrentProgramFocus();
+
+            string displayName = GetProcessDisplayName(process);
+
+            s_dataRepository.LogEntries.Add(new LogEntry(process.Id, process.ProcessName, process.MainWindowTitle, displayName));
+        }
+
+        private static void EndCurrentProgramFocus()
+        {
+            s_dataRepository.LogEntries.LastOrDefault()?.EndProgramFocus();
+        }
+
+        private static string GetProcessDisplayName(Process process)
+        {
+            string displayName = 
+                s_dataRepository.GetProgramByProcessName(process.ProcessName)?.DisplayName ?? "";
+
+            return string.IsNullOrWhiteSpace(displayName) ? process.ProcessName : displayName;
         }
     }
 }
---
+++ ActivityTracker.ViewModels/ProgramFocusSwitchLog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using ActivityTracker.Engine;
+using ActivityTracker.Models;
+using ActivityTracker.Repository;
+
+namespace ActivityTracker.ViewModels
+{
+    public class ProgramFocusSwitchLog
+    {
+        private DataRepository _dataRepository =
+            DataRepository.GetInstance();
+
+        public ObservableCollection<LogEntry> LogEntries =>
+            _dataRepository.LogEntries;
+
+        public void RecordProgramFocusSwitch()
+        {
+            ProcessService.RecordProgramFocusSwitch();
+        }
+    }
+}
